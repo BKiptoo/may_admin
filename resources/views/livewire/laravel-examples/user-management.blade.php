cb77f460fcc5a03fb8b3bfb96aaa2809acc07e7e
BKiptoo <bettsonk@gmail.com>
2024-01-30 23:57:30 +0300

    second push

--- resources/views/livewire/laravel-examples/user-management.blade.php
+++ resources/views/livewire/laravel-examples/user-management.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header pb-0">
                     <div class="d-flex flex-row justify-content-between">
                         <div>
-                            <h5 class="mb-0">All Users</h5>
+                            <h5 class="mb-0">Admins List</h5>
                         </div>
                         <a href="#" class="btn bg-gradient-primary btn-sm mb-0" type="button">+&nbsp; New User</a>
                     </div>

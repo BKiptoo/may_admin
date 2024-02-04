261a117baff56237283af79faa5ed15eb8684a02
BKiptoo <bettsonk@gmail.com>
2024-02-04 19:01:23 +0300

    list

--- resources/views/livewire/Blogs/list.blade.php
+++ resources/views/livewire/Blogs/list.blade.php
@@ -4,58 +4,22 @@
             <div class="col">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Add Blog</h5>
-                        <form class="row g-3" wire:submit.prevent="submit">
-                            <div class="col-md-6">
-                                <label for="category_id" class="form-label">Select Category</label>
-                                <select name="category_id" id="category_id"
-                                        class="form-control"
-                                        required
-                                        wire:model.defer="form.category_id">
-                                    <option value="">Select Category</option>
-                                    <option value="1">Category 1</option>
-                                    <option value="2">Category 2</option>
-                                    <!-- Add your options dynamically here -->
-                                </select>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="name" class="form-label">Blog Title</label>
-                                <input type="text" class="form-control"
-                                       id="name" required placeholder="Enter blog title..."
-                                       wire:model.defer="form.name">
-                            </div>
-                            <div class="col-md-12">
-                                <label for="tags" class="form-label">Blog Tags Separated By Commas</label>
-                                <input type="text"
-                                       class="form-control"
-                                       id="tags" placeholder="Enter tags..."
-                                       wire:model.defer="form.tags">
-                            </div>
-                            <div class="col-md-12">
-                                <label for="description" class="form-label">Blog Description</label>
-                                <textarea id="description"
-                                          class="form-control"
-                                          wire:model.defer="form.description"></textarea>
-                            </div>
-                            <div class="col-md-12">
-                                <label for="photo" class="form-label">Blog Banner</label>
-                                <input type="file"
-                                       class="form-control"
-                                       id="photo" accept="image/*" required placeholder="Upload image..."
-                                       wire:model.defer="form.photo">
-                            </div>
-                            <div wire:loading wire:target="form.photo"><span
-                                    class="spinner-border spinner-border-sm"></span> Uploading
-                            </div>
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary btn-lg float-end"
-                                        wire:loading.class="disabled"
-                                        wire:offline.attr="disabled"><span wire:target="submit"
-                                                                           wire:loading.class="spinner-border spinner-border-lg"></span>
-                                    Save
-                                </button>
-                            </div>
-                        </form>
+                        <h5 class="card-title">Blog List</h5>
+                        <div class="row">
+                            @foreach($blogs as $blog)
+                                <div class="col-md-4 mb-3">
+                                    <div class="card">
+                                        <img src="{{ $blog['photo'] }}" class="card-img-top" alt="{{ $blog['name'] }}">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $blog['name'] }}</h5>
+                                            <p class="card-text">{{ $blog['description'] }}</p>
+                                            <!-- Add any other fields you want to display -->
+                                            <a href="#" class="btn btn-primary">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
